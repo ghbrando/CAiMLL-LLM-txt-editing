5d500e2f0e77e7a4e371dbd09785cc3d3f766e16
ghbrando <155781260+ghbrando@users.noreply.github.com>
2024-12-12 11:25:43 -0800

    Created function writeTo
    writeTo takes in two parameters, the first being a dictionary, and the second is a path for the dictionary to be written in.

--- dict.py
+++ dict.py
@@ -36,5 +36,20 @@ file_154ba492_58d0_4e70_9456_661907d4a916 = {
 }
 
 file_1814117d_1d34_47de_837c_4f7249d42664 = {
-    
+
 }
+
+# how do i do a multi line comment in python? 
+# 
+
+def writeTo(dict, path):
+    with open(path, 'w', encoding='utf-8') as file:
+        for key, value in dict.items():
+            file.write(f"{key} -> {value}\n")
+
+''' 
+ Use this function when you want to write out your dictionary to a file, the first parameter is the dictionary 
+ you want to write out, the second parameter is the path to the file you want to write to
+
+ writeTo(file_0c538c99_5f0e_47a3_95ad_c9d56f12e8ee, 'corrected.txt')
+''' 
