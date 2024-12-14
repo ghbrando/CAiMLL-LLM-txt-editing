32c1fc592145e8cc0e318877312b93971ec75278
ttimohy <143227468+ttimohy@users.noreply.github.com>
2024-12-13 18:10:34 -0800

    Update dict.py

--- dict.py
+++ dict.py
@@ -2,4 +2,13 @@
 acronyms_dict = {
     "AI": "Artificial Intelligence",
     "LLM": "Large Language Model",
+    "Naval X": "NavalX",
+    "NRL": "NREL",
+    "Ari": "ARI",
+    "Propel": "PROPELS",
+    "JREX": "JREC",
+    "Anello": "ANELLO",
+    "Mabel X": "NavalX",
+    "iOS": "IOS",
+    "SIBOR": "SBIR"
 }
